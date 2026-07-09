94f32790bead9a47332cd8a580a5ab20027394f9
zhangfuxing <fuxing.zhang@qq.com>
2026-07-09 12:35:02 +0800

    feat: clickable status bar toggle, about page data folder and copy version
    - Make status bar core status indicator clickable to toggle start/stop
    - Add open data directory and copy version info buttons to About page
    - Add license notice to About page

--- WinUIClash/Views/Settings/AboutView.xaml.cs
+++ WinUIClash/Views/Settings/AboutView.xaml.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace WinUIClash.Views.Settings;
@@ -7,5 +9,29 @@ public sealed partial class AboutView : UserControl
     public AboutView()
     {
         InitializeComponent();
+    }
+
+    private void OpenDataFolder_Click(object sender, RoutedEventArgs e)
+    {
+        var dataDir = System.IO.Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            "WinUIClash");
+
+        if (!System.IO.Directory.Exists(dataDir))
+            System.IO.Directory.CreateDirectory(dataDir);
+
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = dataDir,
+            UseShellExecute = true,
+        });
+    }
+
+    private void CopyVersion_Click(object sender, RoutedEventArgs e)
+    {
+        var version = "WinUIClash v0.1.0 (dev)\n.NET 10 + Windows App SDK 2.2\nClashMeta (Mihomo)";
+        var dp = new Windows.ApplicationModel.DataTransfer.DataPackage();
+        dp.SetText(version);
+        Windows.ApplicationModel.DataTransfer.Clipboard.SetContent(dp);
     }
 }
