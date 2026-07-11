47f98a29735603eda3313f21b07d6353b2e97cde
zhangfuxing <fuxing.zhang@qq.com>
2026-07-11 12:34:57 +0800

    feat(settings): add selection indicator for accent color picker
    - Highlight the currently selected accent color with a 2px black border
    - Apply border directly to Button control to avoid pixel alignment gaps
    - Override PointerOver/Pressed resources to preserve color on hover

--- WinUIClash/Views/Settings/ThemeSettingsView.xaml.cs
+++ WinUIClash/Views/Settings/ThemeSettingsView.xaml.cs
@@ -1,5 +1,7 @@
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
 using WinUIClash.ViewModels.Settings;
 
 namespace WinUIClash.Views.Settings;
@@ -12,6 +14,7 @@ public sealed partial class ThemeSettingsView : UserControl
     {
         ViewModel = ServiceLocator.Get<ThemeSettingsViewModel>();
         InitializeComponent();
+        Loaded += (_, _) => UpdateColorSelection();
     }
 
     private void Color_Click(object sender, RoutedEventArgs e)
@@ -20,6 +23,44 @@ public sealed partial class ThemeSettingsView : UserControl
         {
             var index = Array.IndexOf(ViewModel.PrimaryColors, color);
             if (index >= 0) ViewModel.PrimaryColorIndex = index;
+            UpdateColorSelection();
+        }
+    }
+
+    private void ColorsRepeater_ElementPrepared(ItemsRepeater sender, ItemsRepeaterElementPreparedEventArgs args)
+    {
+        UpdateColorBorder(args.Element, args.Index);
+    }
+
+    private void UpdateColorSelection()
+    {
+        for (int i = 0; i < ViewModel.PrimaryColors.Length; i++)
+        {
+            var element = ColorsRepeater.TryGetElement(i);
+            if (element != null) UpdateColorBorder(element, i);
+        }
+    }
+
+    private void UpdateColorBorder(UIElement element, int index)
+    {
+        if (element is Button btn)
+        {
+            if (index == ViewModel.PrimaryColorIndex)
+            {
+                var blackBrush = new SolidColorBrush(Colors.Black);
+                btn.BorderThickness = new Thickness(2);
+                btn.BorderBrush = blackBrush;
+                btn.Resources["ButtonBorderBrushPointerOver"] = blackBrush;
+                btn.Resources["ButtonBorderBrushPressed"] = blackBrush;
+            }
+            else
+            {
+                var transparentBrush = new SolidColorBrush(Colors.Transparent);
+                btn.BorderThickness = new Thickness(0);
+                btn.BorderBrush = null;
+                btn.Resources["ButtonBorderBrushPointerOver"] = transparentBrush;
+                btn.Resources["ButtonBorderBrushPressed"] = transparentBrush;
+            }
         }
     }
 
